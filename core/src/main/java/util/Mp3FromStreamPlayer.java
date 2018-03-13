3ba98722cac3bd9dbd558e4b4d638556d87aa684
sergiy <sergiy.kunnik@gmail.com>
2018-03-13 13:31:10 +0200

    Реалізував playMp3 і stopMp3

--- core/src/main/java/util/Mp3FromStreamPlayer.java
+++ core/src/main/java/util/Mp3FromStreamPlayer.java
@@ -1,13 +1,24 @@
 package util;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+
+import sun.audio.*;
 
 public class Mp3FromStreamPlayer {
 
     public void playMp3(FileInputStream stream) {
         // TODO: Play mp3 having stream
+        try {
+            AudioStream audioStream = new AudioStream(stream);
+            AudioPlayer.player.start(audioStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-    public void stopMp3(){
+    public void stopMp3(AudioStream audioStream){
         // TODO: Stop mp3 playing rightHere
+        AudioPlayer.player.stop(audioStream);
     }
 }
